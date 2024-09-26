9bc07b529461f5c62f129711bdc5c5915db1bc33
daniel nzioka <dmwinzi50@gmail.com>
2024-09-26 17:38:04 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    my_list=[]
+    if(length == 0):
+        print(my_list)
+    elif(length == 1):
+        my_list.append(0)
+        print(my_list)
+    elif(length == 2):
+        my_list=[0]
+        my_list.insert(1,1)
+        print(my_list)
+    else:
+        my_list=[0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+        print(my_list)
